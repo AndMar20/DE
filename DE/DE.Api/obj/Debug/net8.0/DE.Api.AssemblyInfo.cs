0cd33297a77ee01e33ad125f9dd483cb3e15fe6a
sudzekai <yomakum@mail.ru>
2025-11-26 11:36:27 +0300

    S

--- DE/DE.Api/obj/Debug/net8.0/DE.Api.AssemblyInfo.cs
+++ DE/DE.Api/obj/Debug/net8.0/DE.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DE.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+764e92eecd9d30ef2ac2a3b006d8b79393ea1242")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f3c993e5fc7eff2d0bd34443839ddb198dad7df")]
 [assembly: System.Reflection.AssemblyProductAttribute("DE.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DE.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
